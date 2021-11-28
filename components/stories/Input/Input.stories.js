021f87b549135dd0ae6aac6d435020c66d59735e
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-29 03:49:04 +0700

    Renaming and Sorting Stories

--- components/stories/Input/Input.stories.js
+++ components/stories/Input/Input.stories.js
@@ -9,3 +9,7 @@ export default {
 export const Small = () => <Input size='small' placeholder='Small Size' />;
 export const Medium = () => <Input size='medium' placeholder='Medium Size' />;
 export const Large = () => <Input size='large' placeholder='Large Size' />;
+
+Small.storyName = "Small Input";
+Medium.storyName = "Medium Input";
+Large.storyName = "Large Input";
