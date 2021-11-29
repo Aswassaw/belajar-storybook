80aae5af3790140442850e07ed8082935582c514
Andry Pebrianto <andrypeb227@gmail.com>
2021-11-29 07:07:10 +0700

    Using args

--- components/stories/Button/Button.stories.jsx
+++ components/stories/Button/Button.stories.jsx
@@ -4,9 +4,35 @@ import Button from "./Button";
 export default {
   title: "Form/Button",
   component: Button,
+  args: {
+    children: "Default Children",
+  },
 };
 
+// Menggunakan stories biasa
 export const Primary = () => <Button variant='primary'>Primary</Button>;
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
 export const Success = () => <Button variant='success'>Success</Button>;
 export const Danger = () => <Button variant='danger'>Danger</Button>;
+
+// Menggunakan stories template dan args
+const Template = (args) => <Button {...args} />;
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+  variant: "primary",
+  children: "Primary Args",
+};
+PrimaryA.storyName = "Primary Args";
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+  ...PrimaryA.args,
+  children: "Long Primary Args",
+};
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+  variant: "secondary",
+  // children: "Secondary Args",
+};
